fb8c20cf209fa0cf78e970a098a931ecc1a58397
815000 <815000@SAIT224318IT.ACDM.DS.SAIT.CA>
2020-11-09 12:47:16 -0700

    Logic implemented and functionality tested

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -1,5 +1,6 @@
 package filters;
 
+import dataaccess.UserDB;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -10,13 +11,30 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import models.User;
 
 public class AdminFilter implements Filter {
         
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         
+        // code that is executed before the servlet
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        HttpSession session = httpRequest.getSession();
+        String email = (String)session.getAttribute("email");
+        
+        UserDB userDB = new UserDB();
+        User user = userDB.get(email);
+        
+        if (user.getRole().getRoleId() != 1) {
+            HttpServletResponse httpResponse = (HttpServletResponse)response;
+            httpResponse.sendRedirect("notes");
+            return;
+        }
 
+        chain.doFilter(request, response); // execute the servlet
+
+        // code that is executed after the servlet
 
     }
     
